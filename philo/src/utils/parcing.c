68c830c17bdf83380f980541d201bfc352b3909d
Montaser-asad <montserasad@gmail.com>
2026-06-29 19:39:13 +0300

    started with parsing

--- philo/src/utils/parcing.c
+++ philo/src/utils/parcing.c
@@ -12,7 +12,69 @@
 
 #include "philosophers.h"
 
- parse_input(char *argv[]) 
+static int check_overflow ();
+
+static int	ft_isdigit(int c)
 {
-    
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+static int	ft_isnumber(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str || str[0] == '\0')
+		return (0);
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if(!ft_isdigit((int *) str[i]))
+			return 0;
+		i++;
+	}
+	return 1;
+}
+
+static long atoi (char *str)
+{
+	int	sign;
+    int i;
+    int	result;
+	int	tmp;
+
+	i = 0;
+	result = 0;
+	tmp = result;
+	sign = 1;
+    while (str[i] ==' ' || str[i] == '	' )
+		i++;
+	if (str[i] == '-'|| str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i])
+	{
+		if (ft_isdigit(str[i]) || ft_isnumber(str[i]))
+			return 1;
+		if (tmp != result)
+			return LONG_MAX;
+		result = (result * 10) + str[i] - '0';
+		tmp = result;	
+	}
+
+}
+
+
+void parse_input(char *argv[]) 
+{   
+
+    while ();
 }
